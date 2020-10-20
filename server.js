197dec7d095b39c37aa5853e2c537cf83093842a
Victor Chan <victorcnk@gmail.com>
2020-10-20 22:56:41 +0800

    m

---
+++ server.js
@@ -0,0 +1,59 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const MOVIES = require('./moviedex.json')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+  })
+
+
+function handleGetMovie(req, res) {
+    let response = MOVIES;
+    if (req.query.genre) {
+        response = response.filter(movie =>
+          // case insensitive searching
+          movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+      }
+
+    if (req.query.country) {
+        response = response.filter(movie =>
+          // case insensitive searching
+          movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+    }
+    
+      // filter out greater than or equal to number
+      if (req.query.avg_vote) {
+        response = response.filter(movie =>
+        Number(movie.avg_vote) >= Number(req.query.avg_vote)
+        )
+      }
+    
+      res.json(response)
+    
+}
+
+app.get('/', handleGetMovie)
+
+const PORT = 9000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
